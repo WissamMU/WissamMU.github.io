41f9d09e4163bbc210b6a125ad0cfb5cd3395fa1
wessam mustafa <wessam.2003.ali@gmail.com>
2025-04-24 02:49:06 +0300

    full style resume page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.rtl.min.css';
 import "./css/style.css";
-
+import '@fortawesome/fontawesome-free/js/all.min.js';
 import "./sass/styles.scss";
 
 console.log("Hello World");
+
